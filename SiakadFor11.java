aa50f39f406e4d2505448f394109d7f0b0b2a537
helmi <137972846+teathricalhood122@users.noreply.github.com>
2025-10-23 12:46:01 +0700

    Commit Pertama

---
+++ SiakadFor11.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class SiakadFor11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double nilai, tertinggi = 0, terendah = 100;
+
+        for (int i = 1; i <=10; i++) {
+            System.out.println("Masukan nilai mahasiswa ke-" + i + ":");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi){
+                tertinggi = nilai;
+            }
+            if (nilai < terendah){
+                terendah = nilai;
+            }
+   
+        }
+        System.out.println("Nilai Tertinggi:" + tertinggi);
+        System.out.println("Nilai Terendah:" +terendah);
+    }
+}
